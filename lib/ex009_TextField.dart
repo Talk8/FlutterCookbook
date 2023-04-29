9f88c2d1932d9147583f395e207b7b22421a3742
Talk8 <error365@126.com>
2023-04-29 15:32:10 +0800

    0011 change the code of example 009.

--- lib/ex009_TextField.dart
+++ lib/ex009_TextField.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 
 class ExTextField extends StatefulWidget {
-  const ExTextField({Key?key}):super(key: key);
+  const ExTextField({Key? key}) : super(key: key);
   @override
   State<ExTextField> createState() => _ExTextField();
 }
@@ -33,20 +33,51 @@ class _stateTextField extends State<StateTextField> {
       appBar: AppBar(
         title: Text("Example of TextField"),
       ),
-      body:const TextField(
-        autofocus: true,
-        keyboardType: TextInputType.number,
-        onChanged: (value) => print(value);
-        decoration: InputDecoration(
-          labelText: "Label",
-          hintText: "Name",
-          errorText: "It is wrong",
-          prefixIcon: Icon(Icons.login),
-          suffixIcon: Icon(Icons.panorama_fish_eye),
-          border: OutlineInputBorder(),
+      //如果在这里单独使用TextField,那么无法使用onChanged属性,必须嵌套一个statefull widget.
+      //而且将它包含在布局类widget中，这也是代码中使用column包含它的原因
+      body: const TextFieldStatefull(),
+    );
+  }
+}
+
+class TextFieldStatefull extends StatefulWidget {
+  const TextFieldStatefull({Key? key}) : super(key: key);
+
+  @override
+  State<TextFieldStatefull> createState() => _TextFieldStatefullState();
+}
+
+class _TextFieldStatefullState extends State<TextFieldStatefull> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        const Text("This is the Text Widget"),
+        Container( //Contain可以省略掉，这里只是为了控制TextField的大小
+          alignment: Alignment.center,
+          margin: const EdgeInsets.all(10.0),
+          width: 300,
+          height: 60,
+          child: TextField(
+            autofocus: true,
+            keyboardType: TextInputType.number,
+            onChanged: (value) {
+              print(value);
+            },
+            decoration: const InputDecoration(
+              labelText: "Label",
+              hintText: "Name",
+              errorText: "It is wrong",
+              prefixIcon: Icon(Icons.login),
+              suffixIcon: Icon(Icons.panorama_fish_eye),
+              border: OutlineInputBorder(),
+            ),
+            obscureText: true,
+          ),
         ),
-        // obscureText: true,
-      ),
+        const Icon(Icons.drag_handle_rounded),
+      ],
     );
   }
 }
