26fe3226b6ac095b27f7918cc5b6303c9c63c6fa
Talk8 <error365@126.com>
2023-04-30 11:46:36 +0800

    0012 add the code of FloatingActionButton of Ex010.

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ this is a cookbook of flutter, there are all kinds of demo.
 - ex004_GirdView        ==>     This is the example of GirdView Widget.
 - ex005_Stack           ==>     This is the example of Stack Widget.
 - ex006_RouterNavigator ==>     this is the example of Route and Navigator.
+- ex007_Button          ==>     this is the example of Button Widget.
+- ex008_Text            ==>     this is the example of Text Widget.
+- ex009_TextField       ==>     this is the example of TextField Widget.
+- ex010_BottomNavigationBar==>  this is the example of BottomNavigationBar and FloatingActionButton.
 
--- lib/ex010_BottomNavigation.dart
+++ lib/ex010_BottomNavigation.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:fluttercookbook/ex005_Stack.dart';
+import 'package:fluttercookbook/ex007_Button.dart';
 
+import 'ex008_Text.dart';
+
+//此代码是第17回,18回和第19回的配套代码
 class ExBottomNavigation extends StatefulWidget {
   const ExBottomNavigation({Key? key}) : super(key: key);
   @override
@@ -23,36 +29,72 @@ class BodyWidget extends StatefulWidget {
 
 class _bodyWidget extends State<BodyWidget> {
   int selectIndex = 0;
+
+  final Widget _floatingActionButton = Container(
+    width: 90.0,
+    height: 90.0,
+    padding: const EdgeInsets.all(8),
+    decoration: BoxDecoration(
+      color: Colors.green,
+      borderRadius: BorderRadius.circular(60),
+    ),
+    child: FloatingActionButton(
+      backgroundColor: Colors.purpleAccent,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+        side: const BorderSide(
+          width: 2,
+          color: Colors.black87,
+        ),
+      ),
+      //     //可以是文字或者icon，它会显示在FloatingButton上，默认是蓝色圆形
+      // child: const Text("Float Button"),
+      child: const Icon(Icons.add),
+      onPressed: () {
+        print("FloatingButton onClicked");
+      },
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> bodyWidgetList = const [
+      ExStack(),
+      ExButton(),
+      ExText(),
+    ];
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("BottomNavigationBar Example "),
-        ),
-        body: const Text("test"),
-        bottomNavigationBar: BottomNavigationBar(
-          //超过3个item时需要使用type属性
-          backgroundColor: Colors.amber,
-          type: BottomNavigationBarType.fixed,
-          currentIndex: selectIndex,
-          fixedColor: Colors.blue,
-          //放在iconsize,同时bottomBar整体也跟着放大
-          iconSize: 40,
-          onTap: (index) {
-            setState( () {
-              // print(index);
-              selectIndex = index;
-            });
-          },
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Person"),
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Setting")
-          ],
-        ),
-      );
+      appBar: AppBar(
+        title: const Text("BottomNavigationBar Example "),
+      ),
+      // body: const Text("test"),
+      body: bodyWidgetList[selectIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        //超过3个item时需要使用type属性
+        backgroundColor: Colors.amber,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: selectIndex,
+        fixedColor: Colors.blue,
+        //放在iconsize,同时bottomBar整体也跟着放大
+        iconSize: 40,
+        onTap: (index) {
+          setState(() {
+            // print(index);
+            selectIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Person"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Setting")
+        ],
+      ),
+      floatingActionButton: _floatingActionButton,
+      //控制FloatingActionButton的位置，默认在屏幕右下角
+      //centerFloat和centerDocked的区别在于Docked会让FloatingButton一半位于屏幕，一半位于BottomNavigationBar
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
-
-
-
