93a47cb650ca9920d52729a0937963161a728921
Talk8 <error365@126.com>
2023-06-11 22:29:02 +0800

    0037 change the example of Column and Row.

--- lib/ex001_ColumnRow.dart
+++ lib/ex001_ColumnRow.dart
@@ -14,18 +14,22 @@ class ExColumnRow extends StatelessWidget {
       ),
       body: Container(
         width: 900,
-        height: 100,
+        height: 300,
+        //控制容器内成员的对齐方式，有系统提供的默认值，比如center,也可以自定义
         alignment: Alignment.center,
+        // alignment: Alignment(0.3,-0.6),
         padding: EdgeInsets.all(10),
         margin: EdgeInsets.all(20),
         transform: Matrix4.rotationZ(0.0),
-        transformAlignment: Alignment.bottomCenter,
+        // transformAlignment: Alignment.bottomCenter,
         decoration: BoxDecoration(
             color: Colors.green[300],
             border: Border.all(color: Colors.red, width: 3),
             borderRadius: const BorderRadius.all(Radius.circular(20))),
-        child: Row(
+        child: const Row(
+          //主轴对齐方式，cross表示与主轴垂直方向的对齐方式
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             Expanded(
               child: Column(
