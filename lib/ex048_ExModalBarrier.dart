83431a8bad46b59636693db22ef27b0e6f137f08
Talk8 <error365@126.com>
2023-08-28 14:28:50 +0800

    0110 add the example of modalBarrier.

--- lib/ex048_ExModalBarrier.dart
+++ lib/ex048_ExModalBarrier.dart
@@ -18,28 +18,56 @@ class _ExModalBarrierState extends State<ExModalBarrier> {
         title: const Text('Example of ModalBarrier'),
         backgroundColor: Colors.purpleAccent,
       ),
-      body: Stack(
-        alignment: Alignment.center,
-          children: [
+      body: Stack(alignment: Alignment.center, children: [
         ListView.builder(
             itemCount: 5,
             itemBuilder: (context, index) {
               return SizedBox(height: 60, child: Text(_list[index]));
             }),
         SizedBox(
-          width: 200,
+          width: 400,
           height: 300,
           child: ModalBarrier(
+            ///参数onDismiss源代码写的关闭modal窗口
             // onDismiss: () => Navigator.maybePop(context),
-            // onDismiss: () => Navigator.of(context).pop(),
-            onDismiss: () => Navigator.of(context).removeRoute(ModalRoute.of(context) as Route),
+
+            ///自己写的关闭modal窗口,这两种关闭窗口的方法都会退出当前页面
+            onDismiss: () => Navigator.of(context).pop(),
             color: Colors.blue,
-            semanticsOnTapHint: "hello",
           ),
         ),
-            // if(_showModal) {
-            //   return Text('a');
-            // },
+        ///通过变量控制Modal窗口的显示与隐藏，这样只会关闭Modal窗口，而不会退出当前页面
+        _showModal
+            ? SizedBox(
+                width: 300,
+                height: 400,
+                child: ModalBarrier(
+                  ///点击窗口外的区域无反应，点击窗口内的区域只关闭当前窗口
+                  onDismiss: () {
+                    setState(() {
+                      _showModal = !_showModal;
+                    });
+                  },
+                  color: Colors.green,
+                ),
+              )
+            : const SizedBox.shrink(),
+        ///通过组件控制Modal窗口的显示与隐藏，这样只会关闭Modal窗口，而不会退出当前页面
+        Visibility(
+          visible: _showModal,
+          child: SizedBox(
+            height: 200,
+            width: 300,
+            child: ModalBarrier(
+              color: Colors.yellow,
+              onDismiss: () {
+                setState(() {
+                  _showModal = !_showModal;
+                });
+              },
+            ),
+          ),
+        ),
       ]),
     );
   }
