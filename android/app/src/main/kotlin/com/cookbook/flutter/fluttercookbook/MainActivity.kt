6169ab3a29bf6987757d539a7dad37f90b6219bf
Talk8 <error365@126.com>
2023-08-29 13:56:38 +0800

    0113 add the example of EventChannel.

--- android/app/src/main/kotlin/com/cookbook/flutter/fluttercookbook/MainActivity.kt
+++ android/app/src/main/kotlin/com/cookbook/flutter/fluttercookbook/MainActivity.kt
@@ -7,24 +7,36 @@ import android.content.IntentFilter
 import android.os.BatteryManager
 import android.os.Build
 import android.os.Build.VERSION_CODES
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
+import java.io.OutputStream
+import java.util.logging.Formatter
+import java.util.logging.StreamHandler
 
 class MainActivity: FlutterActivity() {
-    // 1.创建channel
+    private val TAG = "MainActivity"
+    // 1.创建channel：有三种channel:MethodChannel,EventChannel,BasicMessageChannel
     private val channel = "www.acf.com/battery"
+    private val eventChannel = "www.acf.com/event"
     //重写方法来设置chanel监听器，用来监听channel中的方法
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
         //2.通过方法中的flutterEngine获取MethodChannel对象
         val methodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger,channel)
+        val eventChannel = EventChannel(flutterEngine.dartExecutor.binaryMessenger,eventChannel)
 
         //3. 设置监听器用来监听channel中的方法，就是flutter代码中的invokeMethod()方法调用的方法
         methodChannel.setMethodCallHandler { call, result ->
             //判断是否是获取电量的方法，不是则返回错误,返回内容通过监听器中的result参数返回
             if(call.method == "getBattery") {
+                //从arguments参数中获取数据
+                var data = call.arguments
+                Log.d(TAG, "configureFlutterEngine: arguments: "+data.toString())
+
                 val battery = getBattery()
                 if(battery == -1) {
                     result.error("300","unKnowError",null)
@@ -35,6 +47,9 @@ class MainActivity: FlutterActivity() {
                 result.notImplemented()
             }
         }
+
+        var sHandler = StreamHandlerImpl()
+        eventChannel.setStreamHandler(sHandler)
     }
 
     private fun getBattery(): Int {
@@ -51,4 +66,15 @@ class MainActivity: FlutterActivity() {
 //        return  20;
     }
 
+}
+
+//自定义类继承自StreamHandler，重写其中的方法来给Flutter发送数据
+class StreamHandlerImpl : EventChannel.StreamHandler {
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+        events?.success("native event")
+    }
+
+    override fun onCancel(arguments: Any?) {
+        TODO("Not yet implemented")
+    }
 }
--- lib/ex049_ExMethodChannel.dart
+++ lib/ex049_ExMethodChannel.dart
@@ -10,7 +10,13 @@ class ExChannel extends StatefulWidget {
 
 class _ExChannelState extends State<ExChannel> {
   ///创建channel对象时最好使用域名/功能名这样的形式,在原生代码中会获取该channel
-  static const platform = MethodChannel("www.acf.com/battery");
+  ///有三种channel:MethodChannel:可以实现flutter和原生代码双向通信，通信内容通过方法的参数来传递
+  ///EventChannel:主要用来从原生给Flutter发消息
+  ///BasicMessageChannel：可以实现flutter和原生代码双向通信，通信内容为简单字符串等内容
+  static const flutterMethodChannel = MethodChannel("www.acf.com/battery");
+  static const flutterEventChannel = EventChannel("www.acf.com/event");
+  // static const flutterMessageChannel = BasicMessageChannel('name', )
+
   late int batteryLevel = 0;
 
   @override
@@ -25,18 +31,32 @@ class _ExChannelState extends State<ExChannel> {
         children: [
           ElevatedButton(
             onPressed: () => getBattery(),
-            child: const Text('Get')),
+            child: const Text('Get Data From Native platform')),
+          Text('Battery: $batteryLevel'),
+          ElevatedButton(
+              onPressed: () => sendEventChannel(),
+              child: const Text('Get event From Native platform')),
           Text('Battery: $batteryLevel'),
         ],
       ),
     );
   }
 
-  ///调用Native中的方法,方法名就是invokeMethod()方法的参数
+  ///调用Native中的方法,方法名就是invokeMethod()方法的参数,第二个参数用来传递数据
   void getBattery() async {
-    final result = await platform.invokeMethod("getBattery");
+    String data = "send data";
+    final result = await flutterMethodChannel.invokeMethod("getBattery",data);
     setState(() {
       batteryLevel = result;
     });
+  }
+
+  ///原生平台返回的是stream,通完listen方法监听里面发来的event
+  void sendEventChannel() {
+    var streamSubscription = flutterEventChannel.receiveBroadcastStream()
+        .listen((event) { debugPrint("data: ${event.toString()}");},
+    onError: (e) => debugPrint('error: ${e.toString()}'),
+    onDone: () => debugPrint('event done'),
+    cancelOnError: true);
   }
 }
