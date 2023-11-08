a8402466e8926e20649e830ab4d14a8f38761a58
Talk8 <error365@126.com>
2023-11-08 16:21:26 +0800

    0153 fixed the warning of code.

--- lib/ex007_Button.dart
+++ lib/ex007_Button.dart
@@ -1,4 +1,4 @@
-//本代码和第10回的代码匹配，主要介绍Button相关的内容
+///本代码和第10回的代码匹配，主要介绍Button相关的内容
 import 'package:flutter/material.dart';
 
 class ExButton extends StatelessWidget {
@@ -36,32 +36,32 @@ class ExButton extends StatelessWidget {
               ///可以依据相关的条件来disable/enable
               // onPressed: null,
               onPressed: () {},
-              child: Text("ElevatedButton"),
               ///这个style可以button中文字和背景的配色：黑底白字
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.black,
                 foregroundColor: Colors.white,
               ),
+              child: const Text("ElevatedButton"),
             ),
             //同时带有文字和图标的按钮
             TextButton.icon(
-              icon: Icon(
+              icon: const Icon(
                 Icons.add,
                 size: 36.0,
                 color: Colors.lightGreen,
               ),
               onPressed: () {},
-              label: Text("TextButton"),
+              label: const Text("TextButton"),
             ),
             OutlinedButton.icon(
                 onPressed: (){},
-                icon: Icon(Icons.delete),
-                label:Text("Delete"),
+                icon: const Icon(Icons.delete),
+                label:const Text("Delete"),
             ),
             ElevatedButton.icon(
                 onPressed: (){},
-                icon: Icon(Icons.search_rounded),
-                label: Text('search'),
+                icon: const Icon(Icons.search_rounded),
+                label: const Text('search'),
             ),
 
             //水波效果，这个需要使用stack/position添加到某个组件上面
