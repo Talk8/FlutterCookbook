2f411fe5a9561f5cc9a50b06c65ba257bdbba42b
Talk8 <error365@126.com>
2023-11-15 18:20:14 +0800

    0159 add the example of GradientColor.

--- lib/ex069_ExGradientColor.dart
+++ lib/ex069_ExGradientColor.dart
@@ -15,97 +15,100 @@ class _ExGradientColorState extends State<ExGradientColor> {
       appBar: AppBar(
         title: const Text("Example of GradientColor"),
       ),
-      body: Column(
-        children: [
-          const Spacer(),
-          ///线性渐变
-          Container(
-            width: double.infinity,
-            height: 100,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.greenAccent,Colors.redAccent,Colors.amberAccent]
-              )
-            ),
-            child:const SizedBox.shrink(),
-          ),
-          const Spacer(),
-          Container(
-            width: double.infinity,
-            height: 100,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                ///调整开始和结的位置
-                begin: Alignment.topLeft,
-                end: Alignment.center,
-                tileMode: TileMode.repeated,
-                colors: [Colors.greenAccent,Colors.redAccent,Colors.amberAccent]
-              )
-            ),
-            child:const SizedBox.shrink(),
-          ),
-          const Spacer(),
-
-          ///扇形渐变
-          Container(
-            width: double.infinity,
-            height: 100,
-            decoration:const BoxDecoration(
-                gradient: SweepGradient(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 16.0,right: 16.0),
+        child: Column(
+          children: [
+            const Spacer(),
+            ///线性渐变
+            Container(
+              width: double.infinity,
+              height: 100,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
                   colors: [Colors.greenAccent,Colors.redAccent,Colors.amberAccent]
                 )
+              ),
+              child:const SizedBox.shrink(),
             ),
-            child: const SizedBox.shrink(),
-          ),
-
-          const Spacer(),
-          Container(
-            width: double.infinity,
-            height: 100,
-            decoration:const BoxDecoration(
-                gradient: SweepGradient(
-                  center: Alignment.center,
-                  ///设置开始和结的角度
-                  startAngle: math.pi/2,
-                  endAngle: math.pi ,
+            const Spacer(),
+            Container(
+              width: double.infinity,
+              height: 100,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  ///调整开始和结的位置
+                  begin: Alignment.topLeft,
+                  end: Alignment.center,
                   tileMode: TileMode.repeated,
                   colors: [Colors.greenAccent,Colors.redAccent,Colors.amberAccent]
                 )
+              ),
+              child:const SizedBox.shrink(),
             ),
-            child: const SizedBox.shrink(),
-          ),
+            const Spacer(),
 
-          const Spacer(),
-          ///放射形渐变
-          Container(
-            width: double.infinity,
-            height: 100,
-            decoration: const BoxDecoration(
-                gradient: RadialGradient(
-                  colors: [Colors.greenAccent,Colors.redAccent,Colors.amberAccent]
-                )
+            ///扇形渐变
+            Container(
+              width: double.infinity,
+              height: 100,
+              decoration:const BoxDecoration(
+                  gradient: SweepGradient(
+                    colors: [Colors.greenAccent,Colors.redAccent,Colors.amberAccent]
+                  )
+              ),
+              child: const SizedBox.shrink(),
             ),
-            child: const SizedBox.shrink(),
-          ),
-          const Spacer(),
-          Container(
-            width: double.infinity,
-            height: 100,
-            decoration: const BoxDecoration(
-                gradient:RadialGradient(
-                  ///渐变半径：
-                  radius: 0.3,
-                  //渐变位置，主要是中心位置
-                  center: Alignment.center,
-                  tileMode: TileMode.repeated,
-                  colors: [Colors.greenAccent,Colors.redAccent,Colors.amberAccent]
-                  // colors: [Colors.blue.shade100,Colors.blue.shade200,Colors.white,Colors.white,Colors.white],
-                )
+
+            const Spacer(),
+            Container(
+              width: double.infinity,
+              height: 100,
+              decoration:const BoxDecoration(
+                  gradient: SweepGradient(
+                    center: Alignment.center,
+                    ///设置开始和结的角度
+                    startAngle: math.pi/2,
+                    endAngle: math.pi ,
+                    tileMode: TileMode.repeated,
+                    colors: [Colors.greenAccent,Colors.redAccent,Colors.amberAccent]
+                  )
+              ),
+              child: const SizedBox.shrink(),
             ),
-            child: const SizedBox.shrink(),
-          ),
-          const Spacer(),
-        ],
+
+            const Spacer(),
+            ///放射形渐变
+            Container(
+              width: double.infinity,
+              height: 100,
+              decoration: const BoxDecoration(
+                  gradient: RadialGradient(
+                    colors: [Colors.greenAccent,Colors.redAccent,Colors.amberAccent]
+                  )
+              ),
+              child: const SizedBox.shrink(),
+            ),
+            const Spacer(),
+            Container(
+              width: double.infinity,
+              height: 100,
+              decoration: const BoxDecoration(
+                  gradient:RadialGradient(
+                    ///渐变半径：
+                    radius: 0.3,
+                    //渐变位置，主要是中心位置
+                    center: Alignment.center,
+                    tileMode: TileMode.repeated,
+                    colors: [Colors.greenAccent,Colors.redAccent,Colors.amberAccent]
+                    // colors: [Colors.blue.shade100,Colors.blue.shade200,Colors.white,Colors.white,Colors.white],
+                  )
+              ),
+              child: const SizedBox.shrink(),
+            ),
+            const Spacer(),
+          ],
+        ),
       ),
     );
   }
