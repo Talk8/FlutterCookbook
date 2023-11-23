50ea3a7546ab3feb0559d4fa4cbd6040c4104f05
Talk8 <error365@126.com>
2023-11-23 14:29:28 +0800

    0160 add the example of advanced Switch.

--- lib/ex070_ExSlideSwitcher.dart
+++ lib/ex070_ExSlideSwitcher.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:slide_switcher/slide_switcher.dart';
 import 'package:flutter_advanced_switch/flutter_advanced_switch.dart';
@@ -11,15 +12,39 @@ class ExSliderSwitcher extends StatefulWidget {
 }
 
 class _ExSliderSwitcherState extends State<ExSliderSwitcher> {
+  final mController = ValueNotifier<bool>(true);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    mController.addListener(() {
+      setState(() {
+        if(mController.value) {
+          debugPrint("true");
+        }else{
+          debugPrint("false");
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    mController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Example of Slider Switch"),
+        // title: const Text("Example of Slider Switch"),
       ),
       body: Column(
         children: [
-          ///这个组件的脸优点是可以定义开关状态的组件，但是滑动条只能是颜色，不能是组件
+          ///这个组件的优点是可以自定义开关状态的组件，但是滑动条只能是颜色，不能是组件
           SlideSwitcher(
             containerHeight: 50,
             containerWight: 200,
@@ -29,7 +54,7 @@ class _ExSliderSwitcherState extends State<ExSliderSwitcher> {
               Text("Open"),
               Text("Close"),],),
            const SizedBox(height: 16.0,),
-           ///可以垂直显示，但是滑动条无法垂直滑动
+           ///可以垂直显示，而且可以修改开关状态下的图片，不过无法修改thumb的形状
            SlideSwitcher(
             containerHeight: 200,
             containerWight: 60,
@@ -41,19 +66,37 @@ class _ExSliderSwitcherState extends State<ExSliderSwitcher> {
               Icon(Icons.keyboard_arrow_down),],
            ),
           const SizedBox(height: 16.0,),
-          // const AdvancedSwitch(
-          //   activeChild: Icon(Icons.add),
-          //   inactiveChild: Icon(Icons.abc),
-          //   thumb: Icon(Icons.hail),
-          // ),
+
+          ///无法垂直显示，但是可以修改开关状态下的图片和thumb的图片
+          ///需要配合controller使用，不然无法显示开关状态,还有一个缺点，开关无法滑动使用，只能点击开和关位置，自动滑动。
+          const AdvancedSwitch(
+            width: 120,
+            height: 60,
+            // activeChild: Icon(Icons.arrow_left),
+            // inactiveChild: Icon(Icons.arrow_right),
+            // thumb: Icon(Icons.face),
+            enabled: true,
+          ),
           const SizedBox(height: 16.0,),
+
+          AdvancedSwitch(
+            width: 120,
+            controller: mController,
+          ),
+          const SizedBox(height: 16.0,),
+
           Transform.rotate(angle: math.pi/2,
-            ///如何调整大小？
+            ///在外层嵌套一个容器后仍然无法调整switch的大小
+            ///默认的switch无法垂直显示，可以修改开关状态下的图片和thumb的图片
             child: SizedBox(
+              width: 100,
+              height: 60,
               child: Switch(
                 value: true,
                 onChanged: (value){},
-                thumbIcon:MaterialStatePropertyAll<Icon>(Icon(Icons.headset_mic),),
+                activeThumbImage: const AssetImage("images/panda.jpeg"),
+                inactiveThumbImage: const AssetImage("images/ex.png"),
+                thumbIcon:const MaterialStatePropertyAll<Icon>(Icon(Icons.headset_mic),),
               ),
             ),
           ),
@@ -62,7 +105,7 @@ class _ExSliderSwitcherState extends State<ExSliderSwitcher> {
             transform: Matrix4.rotationZ(30),
             transformAlignment:Alignment.center ,
             child: Switch(value: true,onChanged: (value){},),
-          )
+          ),
         ],
       ),
     );
