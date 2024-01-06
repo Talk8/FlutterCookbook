f76c7bf98913b99c034a1af20402dff0e7233862
Talk8 <error365@126.com>
2024-01-06 09:52:32 +0800

    0190 add the package of timezone.

--- lib/ex079_getPkg.dart
+++ lib/ex079_getPkg.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_timezone/flutter_timezone.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:readmore/readmore.dart';
 
 class ExGetPkg extends StatefulWidget {
@@ -14,7 +16,7 @@ class _ExGetPkgState extends State<ExGetPkg> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Example of Package: Get"),
+        title: const Text("Example of Package: Get"),
         backgroundColor: Colors.purpleAccent,
       ),
       body: Column(
@@ -22,7 +24,7 @@ class _ExGetPkgState extends State<ExGetPkg> {
           ElevatedButton(
             onPressed: (){
               Get.showSnackbar(
-                GetSnackBar(
+                const GetSnackBar(
                   title: "Notice",
                   messageText: Text("Today is a holiday"),
                 ),
@@ -30,7 +32,7 @@ class _ExGetPkgState extends State<ExGetPkg> {
             },
             child:const  Text("show SanckBar"),
           ),
-          ReadMoreText(
+          const ReadMoreText(
             "this is a long text,aaaaaaaaaaaaaaaaaaaa,bbbbbbbbbbbbbbb,ccccccccccc,",
             trimLines: 3,
             trimMode: TrimMode.Line,
@@ -40,8 +42,28 @@ class _ExGetPkgState extends State<ExGetPkg> {
 
           ),
 
+          ElevatedButton(onPressed: (){
+            ///获取时区，在future中获取,输出：future timeZone: Asia/Shanghai
+            var timeZone = FlutterTimezone.getLocalTimezone().then((value){
+              debugPrint("future timeZone: ${value.toString()}");
+            });
+
+            ///获取时区，通过异步中获取
+            getTimeZone();
+            ///获取时区，不起作用,需要使用future
+            debugPrint("timeZone: ${timeZone.toString()}");
+
+            ///获取语言区域
+            debugPrint("intl timeZone: ${Intl.getCurrentLocale()}");
+          }, child: const Text("timeZoned"),
+          ),
         ],
       ),
     );
+  }
+  void getTimeZone () async {
+    ///获取时区，输出：async timeZone: Asia/Shanghai
+    var value = await FlutterTimezone.getLocalTimezone();
+    debugPrint("async timeZone: ${value.toString()}");
   }
 }
