2c963655c99c30c3f939b857712e93f0e31d9704
Talk8 <error365@126.com>
2024-02-18 09:29:02 +0800

    0206 add package of timezone.

--- lib/ex079_getPkg.dart
+++ lib/ex079_getPkg.dart
@@ -4,6 +4,8 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:intl/intl.dart';
 import 'package:readmore/readmore.dart';
+import 'package:timezone/timezone.dart'as tz;
+import 'package:timezone/data/latest.dart' as tz;
 
 class ExGetPkg extends StatefulWidget {
   const ExGetPkg({super.key});
@@ -17,6 +19,14 @@ class _ExGetPkgState extends State<ExGetPkg> {
   ///在main中不初始化也读取不到数据.
   final storage = GetStorage("db");
 
+  @override
+  void initState() {
+    super.initState();
+    tz.initializeTimeZones();
+
+  }
+  
+  
   @override
   Widget build(BuildContext context) {
     debugPrint(" read data: ${storage.read("test_key")}");
@@ -48,8 +58,8 @@ class _ExGetPkgState extends State<ExGetPkg> {
 
           ),
 
-          ///获取时区的两种方法
-          ElevatedButton(onPressed: (){
+          ///获取时区的方法，与267与265内容匹配
+          ElevatedButton(onPressed: () async {
             ///获取时区，在future中获取,输出：future timeZone: Asia/Shanghai
             var timeZone = FlutterTimezone.getLocalTimezone().then((value){
               debugPrint("future timeZone: ${value.toString()}");
@@ -59,6 +69,40 @@ class _ExGetPkgState extends State<ExGetPkg> {
             getTimeZone();
             ///获取时区，不起作用,需要使用future
             debugPrint("timeZone: ${timeZone.toString()}");
+
+            ///使用await获取后需要该行代码运行完成后才能运行它后面的代码，注意该方法前面是async修饰。
+            ///如果不加这两行代码，那么intl的Log先输了然后才是timeZone的log
+            var value = await FlutterTimezone.getLocalTimezone();
+            debugPrint("await timeZone: ${value.toString()}");
+
+
+            ///使用timezone包获取时区,它包含了所有的时区，不过没有提供获取时区的接口，
+            ///这个包主要用来转换时区时间
+            var tzLocation = tz.timeZoneDatabase.locations;
+            ///输出时区的key和value，不过都一样，eg:[US/Hawaii - US/Hawaii],这种格式也叫时区标识符
+            ///这个输出的数据库，不过一共有430个而且还在更新
+            for(int i=0; i<tzLocation.values.length; i++) {
+              // debugPrint("tz $i timeZone: [${tzLocation.keys.toList()[i]} - ${tzLocation.values.toList()[i]}]");
+            }
+
+
+            ///BJ时间和lonton时间进行转换 Europe/London - Europe/London]
+            var bjTime= DateTime.now();
+            var bjTimezone = tz.getLocation("Asia/Shanghai");
+            var ldTimezone = tz.getLocation("Europe/London");
+            ///通过时间获取时区
+            var ltTimeZones = ldTimezone.timeZone(bjTime.millisecondsSinceEpoch);
+            var bjTimeZones = bjTimezone.timeZone(bjTime.millisecondsSinceEpoch);
+            debugPrint("bjTimeZone is $bjTimeZones, ltTimeZone: ${ltTimeZones}");
+
+
+            ///转换时间：北京时间8点转换成伦敦时间是几点？通过输出内容可以看到
+            var ltTime = tz.TZDateTime.from(bjTime,ldTimezone);
+            debugPrint("bjTime is $bjTime, ltTime: ${ltTime}");
+
+            ///把2024-1-1-12：13这个时间转换成北京时区下的时间
+            var time = tz.TZDateTime(bjTimezone,2024,1,1,12,13);
+            debugPrint("change to bjTime is $time");
 
             ///获取语言区域
             debugPrint("intl timeZone: ${Intl.getCurrentLocale()}");
