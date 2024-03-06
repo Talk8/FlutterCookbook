f3e2cbf7fd02794929e730010d1eab12202ccdf8
Talk8 <error365@126.com>
2024-03-06 16:55:26 +0800

    0208 add package of lifecycle.

---
+++ lib/ex083_lifecycle_of_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExPageLifeCycle extends StatefulWidget {
+  const ExPageLifeCycle({super.key});
+
+  @override
+  State<ExPageLifeCycle> createState() => _ExPageLifeCycleState();
+}
+
+class _ExPageLifeCycleState extends State<ExPageLifeCycle> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
