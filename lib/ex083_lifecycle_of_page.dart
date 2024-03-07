bec371d78b94aab507c264aa1f402dd2ee181e7b
Talk8 <error365@126.com>
2024-03-07 18:17:33 +0800

    0208 add package of lifecycle.

--- lib/ex083_lifecycle_of_page.dart
+++ lib/ex083_lifecycle_of_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lifecycle/lifecycle.dart';
 
 class ExPageLifeCycle extends StatefulWidget {
   const ExPageLifeCycle({super.key});
@@ -7,9 +8,62 @@ class ExPageLifeCycle extends StatefulWidget {
   State<ExPageLifeCycle> createState() => _ExPageLifeCycleState();
 }
 
-class _ExPageLifeCycleState extends State<ExPageLifeCycle> {
+///LLifecycle的这两个接口还有顺序要求
+class _ExPageLifeCycleState extends State<ExPageLifeCycle> with WidgetsBindingObserver,LifecycleAware,LifecycleMixin{
+  @override
+  void onLifecycleEvent(LifecycleEvent event) {
+    /// TODO: implement onLifecycleEvent
+    debugPrint("life event: $event");
+    ///界面从从出现到消失时事件：visible -> active -> inactive -> invisible
+    switch(event) {
+      case LifecycleEvent.visible:
+        break;
+      case LifecycleEvent.active:
+        break;
+      case LifecycleEvent.inactive:
+        break;
+      case LifecycleEvent.invisible:
+        break;
+      case LifecycleEvent.push:
+        break;
+      case LifecycleEvent.pop:
+        break;
+    }
+  }
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // TODO: implement didChangeAppLifecycleState
+
+    debugPrint(" check state: ${state.toString()}");
+    super.didChangeAppLifecycleState(state);
+  }
+  @override
+  Future<bool> didPopRoute() {
+    // TODO: implement didPopRoute
+    debugPrint(" check didPopRoute ");
+    return super.didPopRoute();
+  }
+
+  @override
+  Future<bool> didPushRoute(String route) {
+    // TODO: implement didPushRoute
+    debugPrint(" check didPushRoute ");
+    return super.didPushRoute(route);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text("Example of LifeCycle"),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: (){
+            Navigator.pushNamed(context, '080Page',);
+          }, child: Text("jump 080"),),
+        ],
+      ),
+    );
   }
 }
