a036834177383b2babe81c6fb40dffb2f012e574
Talk8 <error365@126.com>
2024-05-04 17:08:43 +0800

    0240 add the example of riverpod.

--- lib/ex042_Animation.dart
+++ lib/ex042_Animation.dart
@@ -82,9 +82,9 @@ class _ExAnimationState extends State<ExAnimation> with SingleTickerProviderStat
           ),
           ///自动旋转组件,其它以Animated开头的组件也有动画功能，这类动画是显式动画
           const AnimatedRotation(
-            child: Text("hi"),
             duration: Duration(seconds: 3),
             turns: pi/3,
+            child: Text("hi"),
           ),
           AnimatedOpacity(opacity: 1.0, duration: const Duration(seconds: 3),
             child: Container(width: 100,height: 100,color: Colors.red,),
