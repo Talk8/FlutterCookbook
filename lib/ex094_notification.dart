809ba13beaa21ba31c0416e9818a2f33747a7b2d
Talk8 <error365@126.com>
2024-12-08 16:42:08 +0800

    0258 add the package of notification.

--- lib/ex094_notification.dart
+++ lib/ex094_notification.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 
 class Ex094Notification extends StatefulWidget {
@@ -8,6 +9,79 @@ class Ex094Notification extends StatefulWidget {
 }
 
 class _Ex094NotificationState extends State<Ex094Notification> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    AwesomeNotifications().setListeners(
+      ///这种写法也可以，感觉不够简洁
+      /*
+        onActionReceivedMethod: (ReceivedAction receivedAction) {
+          return NotificationController.onActionReceivedMethod(receivedAction);
+        },
+       */
+      ///第一个参数是必须的，剩下的三个参数是可选的
+      onActionReceivedMethod: NotificationController.onActionReceivedMethod,
+      onNotificationCreatedMethod: NotificationController.onNotificationCreatedMethod,
+      onNotificationDisplayedMethod: NotificationController.onNotificationDisplayedMethod,
+      onDismissActionReceivedMethod: NotificationController.onDismissActionReceivedMethod,
+    );
+    super.initState();
+  }
+
+  ///获取应用发送通知的权限
+  void requestPermissionOfNotification(BuildContext context) {
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed){
+      if(!isAllowed) {
+        debugPrint("Notification permission is not allowed");
+        if(mounted) {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text("Dialog of Notification"),
+                  content: const Text(
+                      "Do you allow the permission of notification ?"),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        debugPrint("Yes selected");
+                        AwesomeNotifications()
+                            .requestPermissionToSendNotifications();
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("Yes"),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        debugPrint("No selected");
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("No"),
+                    ),
+                  ],
+                );
+             }); //builder
+       }
+      } else {
+        debugPrint("Notification permission is allowed");
+      }
+    });
+  }
+
+  void showNotification() {
+    AwesomeNotifications().createNotification(
+        content: NotificationContent(
+          id: 1,
+          ///这个key需要和main方法中注册的key相同，不然会提示找不到key
+          channelKey: "key4channel",
+          actionType: ActionType.Default,
+          // actionType: ActionType.KeepOnTop,
+          title: "Notification Title",
+          body: "this is the body of notification",
+        )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,12 +95,47 @@ class _Ex094NotificationState extends State<Ex094Notification> {
           Positioned(
             top: 120,
             child:ElevatedButton(
-              onPressed: () { },
+              onPressed: () {
+                requestPermissionOfNotification(context);
+              },
+              child: const Text("request notification permission"),
+            ),
+          ),
+
+          Positioned(
+            top: 240,
+            child:ElevatedButton(
+              onPressed: () {
+                showNotification();
+              },
               child: const Text("show notification"),
             ),
           ),
         ],
       ),
     );
+  }
+}
+
+///这个类自定义的，用来响应各种通知事件
+class NotificationController {
+  @pragma("vm:entry-point")
+  static Future<void> onNotificationCreatedMethod(ReceivedNotification receivedNotification) async {
+    debugPrint("onNotificationCreatedMethod");
+  }
+
+  @pragma("vm:entry-point")
+  static Future<void> onNotificationDisplayedMethod(ReceivedNotification receivedNotification) async {
+    debugPrint("onNotificationDisplayedMethod");
+  }
+
+  @pragma("vm:entry-point")
+  static Future<void> onDismissActionReceivedMethod(ReceivedAction receivedAction) async {
+    debugPrint("onDismissActionReceivedMethod");
+  }
+
+  @pragma("vm:entry-point")
+  static Future<void> onActionReceivedMethod(ReceivedAction receivedAction) async {
+    debugPrint("onActionReceivedMethod");
   }
 }
