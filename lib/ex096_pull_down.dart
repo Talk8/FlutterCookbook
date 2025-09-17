6ae7de37e7ec88e7288768a36d9dc0d363b6e048
Talk8 <error365@126.com>
2025-09-17 23:30:22 +0800

    0267  add the package of pull down button.

--- lib/ex096_pull_down.dart
+++ lib/ex096_pull_down.dart
@@ -15,7 +15,21 @@ class _Ex096PullDownState extends State<Ex096PullDown> {
         title: const Text('Example of Sliding up panel'),
         backgroundColor: Colors.purpleAccent,
       ),
-      body: Text("data"),
+      body: Column(
+        children: [
+          TextButton(onPressed: () {
+            showBottomSheet(context: context, builder: (context){
+              return BottomSheet(onClosing: (){}, builder: (context){
+                return Container(
+                  child: Text("bottom sheet"),
+                );
+              });
+
+            });
+          }, child: Text("show bottomSheet"),),
+          Text("data"),
+        ],
+      ),
     );
   }
 }
